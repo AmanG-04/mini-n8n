868d017f5a3bb6a8ec562dbb29218da97bf21571
Aman Gupta <10amangupta04@gmail.com>
2026-08-10 16:09:39 +0530

    return named composite results from workflow rpcs

--- nhost/migrations/default/1710000002000_workflow_rpcs/down.sql
+++ nhost/migrations/default/1710000002000_workflow_rpcs/down.sql
@@ -2,3 +2,5 @@ DROP FUNCTION IF EXISTS public.record_workflow_usage(uuid);
 DROP FUNCTION IF EXISTS public.approve_workflow_step(uuid, uuid);
 DROP FUNCTION IF EXISTS public.start_webhook_workflow_run(uuid, text, jsonb);
 DROP FUNCTION IF EXISTS public.start_workflow_run(uuid, uuid, text, jsonb);
+DROP TYPE IF EXISTS public.workflow_usage_result;
+DROP TYPE IF EXISTS public.workflow_action_result;
